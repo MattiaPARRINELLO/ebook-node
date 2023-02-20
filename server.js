4d19c28b4f9c9d76d125e031992bb676ef424150
Mattia <contactperso.mp@gmail.com>
2023-02-20 19:57:47 +0100

    Repassed to nodejs

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+//public folder
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+app.get("/commencer", (req, res) => {
+  res.sendFile("commencer.html", { root: __dirname });
+});
+app.get("/guide", (req, res) => {
+  res.sendFile("guide.html", { root: __dirname });
+});
+app.get("/statique/:dos/:file", (req, res) => {
+  let dos = req.params.dos;
+  let file = req.params.file;
+  res.sendFile(`/public/statique/${dos}/${file}`, { root: __dirname });
+});
+app.get("/css/:file", (req, res) => {
+  let file = req.params.file;
+  res.sendFile(`/public/css/${file}`, { root: __dirname });
+});
+app.get("/js/:file", (req, res) => {
+  let file = req.params.file;
+  res.sendFile(`/public/js/${file}`, { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
